fb8f960495fd1a39b2a4d980c12bdaae6a1e1b95
graf <graf0wert@mail.ru>
2020-08-26 23:47:57 +0300

    init commit

---
+++ src/main/java/com/lisiy/demoproject/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.lisiy.demoproject.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+//    private final String sharedKey = "SHARED_KEY";
+//
+//    @GetMapping
+//    public String test() {
+//        return "ZAEBIS";
+//    }
+}
