313391498adad298ebca74c45e1940db648979bb
graf <graf0wert@mail.ru>
2020-10-10 18:54:43 +0300

    fixed project

--- src/main/java/com/lisiy/demoproject/controller/RegistrationController.java
+++ src/main/java/com/lisiy/demoproject/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 import java.util.Map;
 
 @Controller
@@ -31,7 +32,6 @@ public class RegistrationController {
         return "redirect:/login";
     }
 
-
     @GetMapping("/activate/{code}")
     public String activate(Model model, @PathVariable String code) {
         boolean isActivated = userService.activateUser(code);
@@ -43,6 +43,10 @@ public class RegistrationController {
         }
 
         return "login";
+    }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
